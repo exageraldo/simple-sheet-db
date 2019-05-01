0ac726f3971dae88a79c5617b9e2c1a67b51e984
Geraldo Castro <victormatheuscastro@gmail.com>
2019-05-01 13:08:32 -0300

    Add table class

---
+++ sheet_db/table.py
@@ -0,0 +1,15 @@
+class SheetTable(object):
+    def __init__(self, table):
+        self.table = table
+    
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def delete(self):
+        pass
