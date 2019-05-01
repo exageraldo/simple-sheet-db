2a2728b5aea9836d89c9c6d111acb70894205d56
Geraldo Castro <victormatheuscastro@gmail.com>
2019-05-01 17:17:15 -0300

    Get a table or create a new one (if upsert)

--- sheet_db/table.py
+++ sheet_db/table.py
@@ -1,6 +1,40 @@
+from gspread.exceptions import WorksheetNotFound
+from sheet_db.utils import ALPHA_COLS
+
+
 class SheetTable(object):
-    def __init__(self, table):
-        self.table = table
+    def __init__(self, table_name, db_sheet, upsert=False, opt=None):
+        self.table_name = table_name
+        self.db = db_sheet
+        self.table = self._get_table(upsert, opt)
+
+    def _get_table(self, upsert, opt):
+        try:
+            table = self.db.worksheet(self.table_name)
+            self.col_count = table.col_count
+            self.row_count = table.row_count
+            _cols = [table.cell(col=n, row=1) for n in range(1, self.col_count+1)]
+            if len(_cols) == 1 and _cols[0] == '':
+                self.cols = []
+            else:
+                self.cols = _cols
+
+            return table
+        except WorksheetNotFound:
+            if upsert:
+                table = self.db.add_worksheet(
+                    table_name,
+                    rows=1,
+                    cols=1
+                )
+                self.col_count = 1
+                self.rows_count = 1
+                self.cols = []
+
+                return table
+            raise
+        except:
+            raise
     
     def create(self):
         pass
